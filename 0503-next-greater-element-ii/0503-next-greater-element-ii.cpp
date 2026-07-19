700c4c6953ecabaffcf76091b3bcf3c97f5f00a9
Udit bhardwaj <uditbhardwaj655@gmail.com>
2026-07-19 16:38:47 +0530

    Time: 0 ms (100%), Space: 28.4 MB (84.21%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            int nge = -1;
-            for (int j = i + 1; j < i + (nums.size()); j++) {
-                int index = j % nums.size();
-                if (nums[index] > nums[i]) {
-                    nge = nums[index];
-                    break;
+
+        int n = nums.size();
+
+        vector<int> ans(n, -1);
+
+        stack<int> st;
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums[i % n]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                if (!st.empty()) {
+                    ans[i] = st.top();
                 }
             }
-            ans.push_back(nge);
+
+            st.push(nums[i % n]);
         }
+
         return ans;
     }
 };
