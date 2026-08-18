cd1623a7ca86abca7c9070e77c43b78edeea0f8e
Udit bhardwaj <uditbhardwaj655@gmail.com>
2026-08-19 00:33:15 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,9 +5,18 @@ public:
         if (s.size() != t.size())
             return false;
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int freq[26] = {0};
 
-        return s == t;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+
+        return true;
     }
 };
