df9dd84c4e026adb501ad67b036b8c5f2e576035
Udit bhardwaj <uditbhardwaj655@gmail.com>
2026-08-29 22:54:05 +0530

    Time: 1 ms (86.49%), Space: 15.4 MB (76.68%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+
+        int n = score.size();
+
+        vector<pair<int, int>> arr;
+
+        // unki actually index value store krne ke liye
+        for (int i = 0; i < n; i++) {
+            arr.push_back({score[i], i});
+        }
+
+        // now logic (unko reverse sort krke )
+        sort(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.end());
+        vector<string> ans(n);
+
+        for (int i = 0; i < n; i++) {
+
+            int index = arr[i].second;
+
+            if (i == 0)
+                ans[index] = "Gold Medal";
+            else if (i == 1)
+                ans[index] = "Silver Medal";
+            else if (i == 2)
+                ans[index] = "Bronze Medal";
+            else
+                ans[index] = to_string(i + 1);
+        }
+
+        return ans;
+    }
+};
