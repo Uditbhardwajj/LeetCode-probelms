718326b0bba2096b15b9185758ad4066efc62a46
Udit bhardwaj <uditbhardwaj655@gmail.com>
2026-09-26 23:45:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (75.9%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int x = 0;
+        while (n > 0) {
+            x += (n % 10);
+            n = n / 10;
+        }
+        return x;
+    }
+};
